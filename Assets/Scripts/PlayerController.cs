9ccd527accd8ed1fb6311019b432098accee1cbb
Gabber235 <gabber200@icloud.com>
2022-09-20 16:47:36 +0200

    Changed to coop version

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float rotateSpeed = 1f;
+    // [SerializeField] private float rotateSpeed = 1f;
     [SerializeField] private Camera mainCamera;
-
+    [SerializeField] private InputActionReference _moveActionReference;
+    
     private float _cameraRotationOffset; // Offset by the camera rotation so up always remains up.
     private Transform _playerTransform;
-    
+    private Vector2 _inputVector = Vector2.zero;
+    private CharacterController _charController;
+
+
+    void Awake() {
+        _charController = GetComponent<CharacterController>();
+    }
+
     // Start is called before the first frame update
     void Start() {
         _cameraRotationOffset = mainCamera.transform.eulerAngles.y; // We use the rotation in world-space.
         _playerTransform = transform;
+        
+        _moveActionReference.action.Enable();
     }
-
+    
     // Update is called once per frame
     void Update() {
+
+        _inputVector =  _moveActionReference.action.ReadValue<Vector2>();
+
         // Direction vector for our movement. We take into account camera rotation to have up always be up as
         // expected while playing. Additionally, we normalize to prevent diagonal movement from being faster.
         Vector3 targetVector = Quaternion.Euler(0, _cameraRotationOffset, 0)
-                               * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"))
+                               * new Vector3(_inputVector.x, 0, _inputVector.y)
                                    .normalized;
 
-        // Only move when needed, also prevents rotating back to default as soon as we let go of the movement keys.
+        // Only move when needed
         if (targetVector.magnitude != 0) {
             CharacterMovement(targetVector);
         }
@@ -30,12 +44,9 @@ public class PlayerController : MonoBehaviour {
 
     private void CharacterMovement(Vector3 targetVector) {
         Vector3 targetPosition = moveSpeed * Time.deltaTime * targetVector + _playerTransform.position;
-        _playerTransform.position = targetPosition;
-        
-        // Have player rotate to the direction they are walking
-        _playerTransform.rotation = Quaternion.RotateTowards(
-            _playerTransform.rotation, 
-            Quaternion.LookRotation(targetVector), 
-            rotateSpeed);
+
+        Vector3 moveDir = transform.TransformDirection(targetVector);
+        _charController.Move(Time.deltaTime * moveSpeed * moveDir);
+
     }
 }
