bedcc5c18b0b98aa4a0cc1d8b0c46cfcf461eeb8
RobbieGodschalk <r.godschalk@student.tue.nl>
2022-09-22 14:11:45 +0200

    New character prefab that allows for attacking
    Attacks in a sphere around the colored sphere. Player 1 attacks using Left Shift, Player 2 with right shift. Button is configurable in the control scheme asset.

--- Assets/Scripts/PlayerAttacking.cs
+++ Assets/Scripts/PlayerAttacking.cs
@@ -1,23 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using Palmmedia.ReportGenerator.Core.Parser.Analysis;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.HID;
 
 public class PlayerAttacking : MonoBehaviour {
 
     [SerializeField] private Transform _attackPos;
     [SerializeField] private float _range;
+    
+    // Coop stuff
+    [SerializeField] private InputActionReference _attackActionReference;
 
     private Vector3 test;
     // Start is called before the first frame update
     void Start() {
-        var test = _attackPos.position;
+        _attackActionReference.action.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) {
+        if (_attackActionReference.action.WasPressedThisFrame()) {
             Collider[] enemyCols = Physics.OverlapSphere(_attackPos.position, _range);
 
             foreach (var col in enemyCols) {
