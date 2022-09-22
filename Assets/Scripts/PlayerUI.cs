f92f4745a2c30c10f456be82bd5d80b11be8782c
Gabber235 <gabber200@icloud.com>
2022-09-22 14:11:58 +0200

    First inventory working

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerUI : MonoBehaviour
 {
@@ -9,15 +12,31 @@ public class PlayerUI : MonoBehaviour
 
     [SerializeField] TMP_Text healthText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] Sprite defaultItemImage;
+    
+    [SerializeField] List<Image> itemImagesList;
+    
+    [SerializeField] List<ItemType> itemTypesList;
+    [SerializeField] List<Sprite> itemSpritesList;
+    
     // Update is called once per frame
     void FixedUpdate()
     {
-        healthText.text = $"{player.health}"; 
+        healthText.text = $"{player.health}";
+        
+        for(var i = 0; i < itemImagesList.Count; i++)
+        {
+            var inventory = player.Inventory;
+            if(inventory.Items.Count > i)
+            {
+                var item = inventory.Items[i];
+                var typeIndex = itemTypesList.IndexOf(item.Type);
+                itemImagesList[i].sprite = typeIndex != -1 && typeIndex < itemSpritesList.Count ? itemSpritesList[typeIndex] : defaultItemImage;
+            }
+            else
+            {
+                itemImagesList[i].sprite = defaultItemImage;
+            }
+        }
     }
 }
