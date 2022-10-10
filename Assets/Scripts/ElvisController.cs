c9f51c8859f222eba036d52e618df0138a3b406e
RobbieGodschalk <r.godschalk@student.tue.nl>
2022-10-10 23:15:02 +0200

    Fixed the issues as much as possible
    Elvis still gets wonky when actually near / in the player, but this would logically be fixed by actual attack logic

--- Assets/Scripts/ElvisController.cs
+++ Assets/Scripts/ElvisController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ElvisController : MonoBehaviour {
     private Vector3 _target;
@@ -13,20 +14,23 @@ public class ElvisController : MonoBehaviour {
 
     [SerializeField] private Vector3 _targetPos;
 
+    [SerializeField] private float navDist;
     // Start is called before the first frame update
     void Awake() {
         players = GameObject.FindObjectsOfType<PlayerController>().Select(p => p.transform).ToList();
-        distToPlayers = new List<float> { 0, 0 };
+        distToPlayers = new List<float> { Mathf.Infinity, Mathf.Infinity };
         _playerCount = players.Count;
+
+        agent.SetDestination(transform.localPosition);
     }
     
-    void FixedUpdate() {
+    void Update() {
         for (int i = 0; i < _playerCount; i++) {
-
+    
             RaycastHit hit;
             Debug.DrawRay(transform.position, players[i].position - transform.position, Color.yellow);
-            // Might want to layermask instead
-            if (Physics.SphereCast(transform.position, 1f, players[i].position - transform.position, out hit)) {
+    
+            if (Physics.SphereCast(transform.position, 1f,players[i].position - transform.position, out hit)) {
                 if (hit.collider.CompareTag("Player")) {
                     distToPlayers[i] = Vector3.Distance(transform.position, players[i].position);
                 }
@@ -35,7 +39,7 @@ public class ElvisController : MonoBehaviour {
                 }
             }
         }
-
+    
         if (float.IsPositiveInfinity(distToPlayers[0]) && float.IsPositiveInfinity(distToPlayers[1])) {
             return;
         }
@@ -43,11 +47,22 @@ public class ElvisController : MonoBehaviour {
             if (distToPlayers[0] <= distToPlayers[1]) {
                 agent.SetDestination(players[0].position);
                 _targetPos = players[0].position;
+                agent.isStopped = false;
             }
             else {
                 agent.SetDestination(players[1].position);
                 _targetPos = players[1].position;
+                agent.isStopped = false;
             }
+        }
+        
+        navDist = agent.remainingDistance;
+        _targetPos = agent.destination;
+        
+        if (agent.remainingDistance < 0.3)
+        {
+            // Attack logic
+            agent.isStopped = true;
         }
     }
 }
