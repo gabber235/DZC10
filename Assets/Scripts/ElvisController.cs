18c5d5b50a9e4796c5b1e51e4a2d1e9a53926423
Gabber235 <gabber200@icloud.com>
2022-10-11 09:39:36 +0200

    Simple fix elvis spotting through walls

--- Assets/Scripts/ElvisController.cs
+++ Assets/Scripts/ElvisController.cs
@@ -1,69 +1,74 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ElvisController : MonoBehaviour {
-    private Vector3 _target;
-    public UnityEngine.AI.NavMeshAgent agent;
+public class ElvisController : MonoBehaviour
+{
+    public NavMeshAgent agent;
     [SerializeField] private List<Transform> players;
     [SerializeField] private List<float> distToPlayers;
+
+    [SerializeField] private bool debug;
     private int _playerCount;
+    private Vector3 _target;
 
-    [SerializeField] private Vector3 _targetPos;
+    private Vector3 _targetPos;
 
-    [SerializeField] private float navDist;
     // Start is called before the first frame update
-    void Awake() {
-        players = GameObject.FindObjectsOfType<PlayerController>().Select(p => p.transform).ToList();
-        distToPlayers = new List<float> { Mathf.Infinity, Mathf.Infinity };
+    private void Awake()
+    {
+        players = FindObjectsOfType<PlayerController>().Select(p => p.transform).ToList();
+        distToPlayers = players.Select(_ => Mathf.Infinity).ToList();
         _playerCount = players.Count;
 
         agent.SetDestination(transform.localPosition);
     }
-    
-    void Update() {
-        for (int i = 0; i < _playerCount; i++) {
-    
-            RaycastHit hit;
-            Debug.DrawRay(transform.position, players[i].position - transform.position, Color.yellow);
-    
-            if (Physics.SphereCast(transform.position, 1f,players[i].position - transform.position, out hit)) {
-                if (hit.collider.CompareTag("Player")) {
-                    distToPlayers[i] = Vector3.Distance(transform.position, players[i].position);
-                }
-                else {
-                    distToPlayers[i] = Mathf.Infinity;
-                }
-            }
-        }
-    
-        if (float.IsPositiveInfinity(distToPlayers[0]) && float.IsPositiveInfinity(distToPlayers[1])) {
-            return;
-        }
-        else {
-            if (distToPlayers[0] <= distToPlayers[1]) {
-                agent.SetDestination(players[0].position);
-                _targetPos = players[0].position;
-                agent.isStopped = false;
-            }
-            else {
-                agent.SetDestination(players[1].position);
-                _targetPos = players[1].position;
-                agent.isStopped = false;
-            }
-        }
-        
-        navDist = agent.remainingDistance;
-        _targetPos = agent.destination;
-        
-        if (agent.remainingDistance < 0.3)
+
+    private void FixedUpdate()
+    {
+        var pos = transform.position;
+        for (var i = 0; i < _playerCount; i++)
         {
+            RaycastHit hit;
+#if UNITY_EDITOR
+            if (debug) Debug.DrawRay(pos, players[i].position - pos, Color.yellow);
+#endif
+
+            if (!Physics.SphereCast(transform.position, 0.5f, players[i].position - transform.position, out hit))
+                continue;
+
+            if (hit.collider.CompareTag("Player"))
+                distToPlayers[i] = Vector3.Distance(transform.position, players[i].position);
+            else
+                distToPlayers[i] = Mathf.Infinity;
+        }
+
+        if (float.IsPositiveInfinity(distToPlayers[0]) && float.IsPositiveInfinity(distToPlayers[1])) return;
+
+        var min = distToPlayers.Min();
+        var closestIndex = distToPlayers.FindIndex(d => Math.Abs(d - min) < 0.001f);
+
+        if (closestIndex != -1)
+        {
+            agent.SetDestination(players[closestIndex].position);
+            _targetPos = players[closestIndex].position;
+            agent.isStopped = false;
+            _targetPos = agent.destination;
+        }
+
+        if (agent.remainingDistance < 1)
             // Attack logic
             agent.isStopped = true;
-        }
     }
-}
 
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (!debug) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_targetPos, 0.5f);
+    }
+#endif
+}
